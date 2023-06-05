4a2904d8d4985985db50cbd2e2e6ac3bd94f34e5
Joshua Nunn <joshuanunn@hotmail.co.uk>
2023-06-05 23:46:52 +0100

    Add basic REPL

---
+++ monkey-plusplus/repl.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "repl.hpp"
+#include "lexer.hpp"
+#include "token.hpp"
+
+int main () {
+    std::string line;
+
+    while (true) {
+        std::cout << PROMPT;
+
+        std::getline(std::cin, line);
+
+        auto l = Lexer(line);
+
+        for (auto tok = l.next_token(); tok.type != TokenType::ENDOFFILE; tok = l.next_token()) {
+            std::cout << tok.literal << std::endl;
+        }
+    }
+}
---
+++ monkey-plusplus/repl.hpp
@@ -0,0 +1,8 @@
+#ifndef MONKEY_PLUSPLUS_REPL_HPP
+#define MONKEY_PLUSPLUS_REPL_HPP
+
+#include <string>
+
+static const std::string PROMPT {">> "};
+
+#endif //MONKEY_PLUSPLUS_REPL_HPP
