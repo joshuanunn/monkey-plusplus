d466c6ddfc87ec0295fa724c61e58c455b7db06a
Joshua Nunn <joshuanunn@hotmail.co.uk>
2023-08-11 09:11:28 +0100

    Fix failing ast test by updating LetStatement construction

--- monkey-plusplus/ast.test.cpp
+++ monkey-plusplus/ast.test.cpp
@@ -12,15 +12,12 @@ TEST_CASE("Test String") {
 
     auto program = Program();
 
-    program.statements.push_back(
-            std::make_unique<LetStatement>(
-                    LetStatement(
-                            Identifier(Token{TokenType::IDENT, "myVar"}, "myVar"),
-                            std::make_unique<Identifier>(
-                                    Identifier(Token{TokenType::IDENT, "anotherVar"}, "anotherVar"))
-                    )
-            )
-    );
+    auto stmt = std::make_unique<LetStatement>(LetStatement{Token{TokenType::LET, "let"}});
+
+    stmt->name = std::make_unique<Identifier>(Identifier(Token{TokenType::IDENT, "myVar"}, "myVar"));
+    stmt->value = std::make_shared<Identifier>(Identifier(Token{TokenType::IDENT, "anotherVar"}, "anotherVar"));
+
+    program.statements.push_back(std::move(stmt));
 
     if (program.string() != "let myVar = anotherVar;") {
         std::cerr << "program.string wrong. got=" << program.string() << std::endl;
