f63694708f7804bbabf9e4ca5ae7815c40b1c86a
Joshua Nunn <joshuanunn@hotmail.co.uk>
2023-08-11 09:39:34 +0100

    Update REPL to use Parser

--- monkey-plusplus/repl.cpp
+++ monkey-plusplus/repl.cpp
@@ -2,7 +2,7 @@
 
 #include "repl.hpp"
 #include "lexer.hpp"
-#include "token.hpp"
+#include "parser.hpp"
 
 void start_repl() {
     std::string line;
@@ -12,10 +12,25 @@ void start_repl() {
 
         std::getline(std::cin, line);
 
-        auto l = Lexer(line);
+        auto l = std::make_unique<Lexer>(Lexer(line));
+        auto p = Parser(std::move(l));
 
-        for (auto tok = l.next_token(); tok.type != TokenType::ENDOFFILE; tok = l.next_token()) {
-            std::cout << tok.literal << std::endl;
+        auto program = p.parse_program();
+        if (!p.errors().empty()) {
+            print_parser_errors(p.errors());
+            continue;
         }
+
+        std::cout << program->string() << std::endl;
+    }
+}
+
+void print_parser_errors(std::vector<std::string> errors) {
+    std::cout << MONKEY_FACE << std::endl;
+    std::cout << "Woops! We ran into some monkey business here!" << std::endl;
+    std::cout << " parser errors:" << std::endl;
+
+    for (const auto &msg: errors) {
+        std::cout << "\t" + msg << std::endl;
     }
 }
--- monkey-plusplus/repl.hpp
+++ monkey-plusplus/repl.hpp
@@ -1,10 +1,25 @@
 #ifndef MONKEY_PLUSPLUS_REPL_HPP
 #define MONKEY_PLUSPLUS_REPL_HPP
 
+#include <vector>
 #include <string>
 
 static const std::string PROMPT{">> "};
 
+static const std::string MONKEY_FACE{R"(            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----')"};
+
 void start_repl();
+
+void print_parser_errors(std::vector<std::string> errors);
 
 #endif //MONKEY_PLUSPLUS_REPL_HPP
