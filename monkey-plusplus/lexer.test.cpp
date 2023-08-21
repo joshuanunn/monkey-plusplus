9e9518d4337df361fef1a67d70ee7622f505ac02
Joshua Nunn <joshuanunn@hotmail.co.uk>
2023-08-21 14:20:14 +0100

    Move Token tests from Lexer test to new file

--- monkey-plusplus/lexer.test.cpp
+++ monkey-plusplus/lexer.test.cpp
@@ -134,31 +134,3 @@ if (5 < 10) {
         REQUIRE(tok == t);
     }
 }
-
-TEST_CASE("Test Token Equivalence Semantics") {
-    auto tok1 = Token{TokenType::IDENT, "five"};
-    auto tok2 = Token{TokenType::IDENT, "ten"};
-    auto tok3 = Token{TokenType::IDENT, "five"};
-    auto tok4 = Token{TokenType::STRING, "five"};
-    auto tok5 = Token{TokenType::STRING, "five"};
-
-    REQUIRE(tok1 != tok2);
-    REQUIRE(tok1 == tok3);
-    REQUIRE(tok1 != tok4);
-    REQUIRE(tok1 != tok5);
-    REQUIRE(tok2 != tok3);
-    REQUIRE(tok2 != tok4);
-    REQUIRE(tok2 != tok5);
-    REQUIRE(tok3 != tok4);
-    REQUIRE(tok3 != tok5);
-    REQUIRE(tok4 == tok5);
-
-    tok2.literal = "five";
-    tok4.type = TokenType::IDENT;
-    tok5.type = TokenType::IDENT;
-
-    REQUIRE(tok1 == tok2);
-    REQUIRE(tok1 == tok3);
-    REQUIRE(tok1 == tok4);
-    REQUIRE(tok1 == tok5);
-}
